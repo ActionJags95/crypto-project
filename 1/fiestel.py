ba35058e1a18348e3ca21773522251b661f3b0e8
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-04-07 20:28:57 +0530

    Added existing files

---
+++ 1/fiestel.py
@@ -0,0 +1,68 @@
+from random import randint
+
+def string_to_binary(text):
+    return ''.join(format(ord(char), '08b') for char in text)
+
+def binary_to_string(binaryString):
+    chars = [binaryString[i:i+8] for i in range(0, len(binaryString), 8)]
+    return ''.join(chr(int(char, 2)) for char in chars)
+
+def xor_strings(left, right):
+    return ''.join(str(int(a) ^ int(b)) for a, b in zip(left, right))
+
+def feistel_function(right, key):
+    # You can replace this with something more complex later
+    return xor_strings(right, key)
+
+def feistel_round(left, right, key):
+    feistelResult = feistel_function(right, key)
+    newLeft = right
+    newRight = xor_strings(left, feistelResult)
+    return newLeft, newRight
+
+def generate_keys(initialKey, numRounds, keyLength):
+    keys = [initialKey]
+    for i in range(1, numRounds):
+        prevKey = keys[-1]
+        rotatedKey = prevKey[i % keyLength:] + prevKey[:i % keyLength]
+        keys.append(rotatedKey)
+    return keys
+
+def feistel_encrypt(text, initialKey, numRounds):
+    binaryText = string_to_binary(text)
+    if len(binaryText) % 2 != 0:
+        binaryText += '0'
+    
+    halfLength = len(binaryText) // 2
+    left, right = binaryText[:halfLength], binaryText[halfLength:]
+    keys = generate_keys(initialKey, numRounds, len(initialKey))
+
+    for key in keys:
+        left, right = feistel_round(left, right, key)
+    
+    encryptedBinary = right + left
+    return binary_to_string(encryptedBinary)
+
+def feistel_decrypt(encryptedText, initialKey, numRounds):
+    encryptedBinary = string_to_binary(encryptedText)
+    halfLength = len(encryptedBinary) // 2
+    left, right = encryptedBinary[:halfLength], encryptedBinary[halfLength:]
+    keys = generate_keys(initialKey, numRounds, len(initialKey))
+
+    for key in reversed(keys):
+        left, right = feistel_round(left, right, key)
+    
+    decryptedBinary = right + left  # Reverse the final swap
+    return binary_to_string(decryptedBinary)
+
+if __name__ == "__main__":
+    message = input("Enter the string : ")
+    numRounds = int(input("Enter the number of rounds : "))
+    keyString = ''.join(chr(ord('a')+randint(0,25)) for i in range(1, len(message)))
+    initialKey = string_to_binary(keyString)  # 32-bit binary key
+
+    encryptedText = feistel_encrypt(message, initialKey, numRounds)
+    print("Encrypted:", repr(encryptedText))
+
+    decryptedText = feistel_decrypt(encryptedText, initialKey, numRounds)
+    print("Decrypted:", decryptedText)
